4c152cbd65eda191930fa412bd764fdabc289b43
Angel Hernandez Diaz <ahernandez11diaz@gmail.com>
2021-12-27 10:10:18 -0700

    first commit

---
+++ src/Pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styling/Contact.css';
+
+function Contact() {
+    return (
+        <div className='contact'>
+            
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/Pages/LatestWork.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styling/LatestWork.css';
+
+function LatestWork() {
+    return (
+        <div className='latestWork'>
+            
+        </div>
+    )
+}
+
+export default LatestWork

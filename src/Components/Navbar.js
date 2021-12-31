c254ee3d543d0cf5521ad554594f587b80c6a833
Angel Hernandez Diaz <ahernandez11diaz@gmail.com>
2021-12-30 18:36:04 -0700

    adding contact page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../Styling/Navbar.css';
 import { NavLink } from 'react-router-dom';
 
 
 function Navbar() {
-    const [nav, setNav] = useState(true);
-    const showNav = () => setNav(!nav)
-
     return (
         <>
         <div  className='navbar'>
@@ -14,26 +11,18 @@ function Navbar() {
             <div className='logo__container'>
                 <NavLink exact to='/' className='nav__logo'> <img className='logo__img' src='./Assets/main__logo.png' alt='' /> </NavLink>
             </div>
-            
-            {/* <div onClick={showNav} className={nav ? 'menuToggle__container inactive' : 'menuToggle__container'}>
-                <p className='menu__toggle'>MENU</p>
-                <span  className='toggle__icon'> <img src='./Assets/forward__arrow__icon.png' alt='' />  </span>
-            </div> */}
-            {/* central page links */}
-            <div className={nav ? 'navlink__container active' : 'navlink__container'}>
+            {/* Nav Links */}
+            <div className='navlink__container'>
                 <ul className='navlink__wrapper'>
-                    <li className='nav__item'>
+                    {/* <li className='nav__item'>
                         <NavLink exact to='/Featured' className='nav__link'>Featured</NavLink>
-                    </li>
+                    </li> */}
                     <li className='nav__item'>
                         <NavLink exact to='/Latest-Work' className='nav__link'>Latest Work</NavLink>
                     </li>                        
                     <li className='nav__item'>
                         <NavLink exact to='/Contact' className='nav__link'>Contact</NavLink>
                     </li>
-                    {/* <li className='nav__item'>
-                        <img onClick={showNav} className='toggle__icon__close' src='./Assets/close__icon.png' alt='' />
-                    </li> */}
                 </ul>
             </div>
         </div>
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,24 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styling/Contact.css';
+import emailjs from 'emailjs-com';
 
 function Contact() {
+    function HandleSubmit(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_btpqiei', 'template_85l6ymh', e.target, 'user_VqB97g7ee60J4wAdbqLjK')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+
+        setSuccess(!success)
+    }
+
+    const [ success, setSuccess ] = useState(false);
+
+    const submit = success => {
+        setSuccess(!success);
+    };
+
     return (
         <div className='contact'>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
-            <h1>Contact Page</h1>
+            <img className='contact__img' src='./Assets/contact__img.jpg' alt='' />
+            <div className='form__container'>
+                {success == false && (
+                <form className='contact__form' onSubmit={HandleSubmit}>
+                <h1 className='form__title'>Get In Touch</h1>
+                    <span className='form__row'>
+                        <span className='row__item'>
+                            <label>*FIRST NAME.</label>
+                            <input type='text' required name='fName' />
+                        </span>
+                        <span className='row__item'>
+                            <label>*LAST NAME.</label>
+                            <input type='text' required name='lName' />
+                        </span>
+                    </span>
+                    <span className='form__row'>
+                        <span className='row__item'>
+                            <label>*EMAIL.</label>
+                            <input type='text' required name='email' />
+                        </span>
+                        <span className='row__item'>
+                            <label>*PHONE.</label>
+                            <input type='text'required name='phone'/>
+                        </span>
+                    </span>
+                    <span className='row__item'>
+                        <label>LEAVE A MESSAGE</label>
+                        <textarea name='moreInfo' />
+                    </span> 
+                    <span className='submit'>
+                        <button onClick={submit}>Submit</button>
+                        <img src='./Assets/straight__arrow__icon.png' alt='' />
+                    </span>
+                    <p className='email__link'>EMAIL ME</p>
+                    <a href='mailto:ahernandez11diaz@gmail.com' className='email__link'>Gabby.hdz2016@gmail.com</a> 
+                </form>
+                )}
+                {success == true && (
+                    <span className='submitSuccess__wrapper'>
+                    <h1 className='submitSuccess_title'>Get In Touch</h1>
+                    <p className='submitted'>Thanks for reaching out! I'm thrilled to hear from you.</p>
+                    <p className='email__link'>EMAIL ME</p>
+                    <a href='mailto:ahernandez11diaz@gmail.com' className='email__link'>Gabby.hdz2016@gmail.com</a> 
+                    </span>
+                )}
+            </div>
         </div>
     )
 }
